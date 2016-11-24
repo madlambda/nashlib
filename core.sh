4cc0074ac66aec140f52714103810fc4d98554af
Tiago Natel de Moura <tiago.natel@neoway.com.br>
2016-11-24 11:35:42 -0200

    add range helper
    Signed-off-by: Tiago Natel de Moura <tiago.natel@neoway.com.br>

--- core.sh
+++ core.sh
@@ -5,3 +5,17 @@ fn pwd() {
 
 	return $curdir
 }
+
+fn range(lst) {
+	sz <= len($lst)
+
+	if $sz == "0" {
+		return ()
+	}
+
+	last <= -expr $sz - 0
+	idxs <= seq 0 $last
+	idxs <= split($idxs, "\n")
+
+	return $idxs
+}
